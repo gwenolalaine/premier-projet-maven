9e1797387d4eac60beb9e448b8e62e4c520601f9
Gwenola LAINE <laineg.contact@gmail.com>
2017-09-28 15:28:27 +0200

    profiles

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,7 @@
 package dev;
 
+import java.util.ResourceBundle;
+
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -10,9 +12,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        String asciiArt = FigletFont.convertOneLine("App");
-        System.out.println(asciiArt);
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
+    	System.out.println(asciiArt);
+    	
+    	String environnement = ResourceBundle.getBundle("application").getString("environnement");
+    	System.out.println("Environnement : " + environnement);
+
 	
     }
 }
